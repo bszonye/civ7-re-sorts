005f55549b04c05d104485fc2eee3f9ada194e19
B. Szonye <bszonye@gmail.com>
2025-04-27 08:48:00 -0700

    revise config options

--- ui/options/bz-re-sorts-options.js
+++ ui/options/bz-re-sorts-options.js
@@ -5,47 +5,103 @@ import ModSettings from '/bz-re-sorts/ui/options/mod-options-decorator.js';
 
 const MOD_ID = "bz-re-sorts";
 
+export var bzSortOrder;
+(function (bzSortOrder) {
+        bzSortOrder[bzSortOrder["NAME"] = 0] = "NAME";
+        bzSortOrder[bzSortOrder["SLOTS"] = 1] = "SLOTS";
+        bzSortOrder[bzSortOrder["YIELD_FOOD"] = 2] = "YIELD_FOOD";
+        bzSortOrder[bzSortOrder["YIELD_PRODUCTION"] = 2] = "YIELD_PRODUCTION";
+        bzSortOrder[bzSortOrder["YIELD_GOLD"] = 2] = "YIELD_GOLD";
+        bzSortOrder[bzSortOrder["YIELD_SCIENCE"] = 2] = "YIELD_SCIENCE";
+        bzSortOrder[bzSortOrder["YIELD_CULTURE"] = 2] = "YIELD_CULTURE";
+        bzSortOrder[bzSortOrder["YIELD_HAPPINESS"] = 2] = "YIELD_HAPPINESS";
+        bzSortOrder[bzSortOrder["YIELD_DIPLOMACY"] = 2] = "YIELD_DIPLOMACY";
+})(bzSortOrder || (bzSortOrder = {}));
+const sortOrderOptions = [
+    { label: 'LOC_OPTIONS_BZ_RE_SORTS_SORT_BY_NAME', value: bzSortOrder.NAME },
+    { label: 'LOC_OPTIONS_BZ_RE_SORTS_SORT_BY_SLOTS', value: bzSortOrder.SLOTS },
+    { label: 'LOC_YIELD_FOOD', value: bzSortOrder.YIELD_FOOD },
+    { label: 'LOC_YIELD_PRODUCTION', value: bzSortOrder.YIELD_PRODUCTION },
+    { label: 'LOC_YIELD_GOLD', value: bzSortOrder.YIELD_GOLD },
+    { label: 'LOC_YIELD_SCIENCE', value: bzSortOrder.YIELD_SCIENCE },
+    { label: 'LOC_YIELD_CULTURE', value: bzSortOrder.YIELD_CULTURE },
+    { label: 'LOC_YIELD_HAPPINESS', value: bzSortOrder.YIELD_HAPPINESS },
+    { label: 'LOC_YIELD_DIPLOMACY', value: bzSortOrder.YIELD_DIPLOMACY },
+];
 const BZ_DEFAULT_OPTIONS = {
-    sortCitiesByType: true,
-    sortCitiesByRequirement: true,
-    sortCitiesBySlots: false,
+    sortOrder: bzSortOrder.SLOTS,
+    groupByType: true,
+    groupByReq: true,
 };
 const bzReSortsOptions = new class {
     data = { ...BZ_DEFAULT_OPTIONS };
     constructor() {
         const modSettings = ModSettings.load(MOD_ID);
-        if (modSettings) this.data = modSettings;
+        this.data = {
+            sortOrder:
+            modSettings.sortOrder ??
+            BZ_DEFAULT_OPTIONS.sortOrder,
+            groupByType:
+            modSettings.groupByType ??
+            modSettings.sortCitiesByType ??  // legacy
+            BZ_DEFAULT_OPTIONS.groupByType,
+            groupByReq:
+            modSettings.groupByReq ??
+            modSettings.sortCitiesByRequirement ??  // legacy
+            BZ_DEFAULT_OPTIONS.groupByReq,
+        }
+        console.warn(`DATA bz-re-sorts=${JSON.stringify(this.data)}`);
     }
     save() {
         ModSettings.save(MOD_ID, this.data);
     }
-    get sortCitiesByRequirement() {
-        return this.data.sortCitiesByRequirement ??  BZ_DEFAULT_OPTIONS.sortCitiesByRequirement;
+    get groupByReq() {
+        return this.data.groupByReq;
     }
-    set sortCitiesByRequirement(flag) {
-        this.data.sortCitiesByRequirement = !!flag;
+    set groupByReq(flag) {
+        this.data.groupByReq = !!flag;
         this.save();
     }
-    get sortCitiesBySlots() {
-        return this.data.sortCitiesBySlots ??  BZ_DEFAULT_OPTIONS.sortCitiesBySlots;
+    get groupByType() {
+        return this.data.groupByType;
     }
-    set sortCitiesBySlots(flag) {
-        this.data.sortCitiesBySlots = !!flag;
+    set groupByType(flag) {
+        this.data.groupByType = !!flag;
         this.save();
     }
-    get sortCitiesByType() {
-        return this.data.sortCitiesByType ??  BZ_DEFAULT_OPTIONS.sortCitiesByType;
+    get sortOrder() {
+        return this.data.sortOrder;
     }
-    set sortCitiesByType(flag) {
-        this.data.sortCitiesByType = !!flag;
+    set sortOrder(order) {
+        this.data.sortOrder = order;
         this.save();
     }
 };
-const onInitSortCitiesByType = (info) => {
-    info.currentValue = bzReSortsOptions.sortCitiesByType;
+const onInitSortOrder = (info) => {
+    info.selectedItemIndex = bzReSortsOptions.sortOrder;
 };
-const onUpdateSortCitiesByType = (_info, flag) => {
-    bzReSortsOptions.sortCitiesByType = flag;
+const onUpdateSortOrder = (_info, order) => {
+    bzReSortsOptions.sortOrder = order;
+};
+Options.addInitCallback(() => {
+    Options.addOption({
+        category: CategoryType.Mods,
+        // @ts-ignore
+        group: "bz_mods",
+        type: OptionType.Dropdown,
+        id: "bz-re-sorts-order",
+        initListener: onInitSortOrder,
+        updateListener: onUpdateSortOrder,
+        label: "LOC_OPTIONS_BZ_RE_SORTS_SORT_ORDER",
+        description: "LOC_OPTIONS_BZ_RE_SORTS_SORT_ORDER_DESCRIPTION",
+        dropdownItems: sortOrderOptions,
+    });
+});
+const onInitGroupByType = (info) => {
+    info.currentValue = bzReSortsOptions.groupByType;
+};
+const onUpdateGroupByType = (_info, flag) => {
+    bzReSortsOptions.groupByType = flag;
 };
 Options.addInitCallback(() => {
     Options.addOption({
@@ -53,17 +109,17 @@ Options.addInitCallback(() => {
         // @ts-ignore
         group: "bz_mods",
         type: OptionType.Checkbox,
-        id: "bz-sort-cities-by-type",
-        initListener: onInitSortCitiesByType,
-        updateListener: onUpdateSortCitiesByType,
-        label: "LOC_OPTIONS_BZ_SORT_CITIES_BY_TYPE",
-        description: "LOC_OPTIONS_BZ_SORT_CITIES_BY_TYPE_DESCRIPTION",
+        id: "bz-re-sorts-group-by-type",
+        initListener: onInitGroupByType,
+        updateListener: onUpdateGroupByType,
+        label: "LOC_OPTIONS_BZ_RE_SORTS_GROUP_BY_TYPE",
+        description: "LOC_OPTIONS_BZ_RE_SORTS_GROUP_BY_TYPE_DESCRIPTION",
     });
 });
-const onInitSortCitiesByRequirement = (info) => {
+const onInitGroupByReq = (info) => {
     info.currentValue = bzReSortsOptions.sortCitiesByRequirement;
 };
-const onUpdateSortCitiesByRequirement = (_info, flag) => {
+const onUpdateGroupByReq = (_info, flag) => {
     bzReSortsOptions.sortCitiesByRequirement = flag;
 };
 Options.addInitCallback(() => {
@@ -72,30 +128,11 @@ Options.addInitCallback(() => {
         // @ts-ignore
         group: "bz_mods",
         type: OptionType.Checkbox,
-        id: "bz-sort-cities-by-requirement",
-        initListener: onInitSortCitiesByRequirement,
-        updateListener: onUpdateSortCitiesByRequirement,
-        label: "LOC_OPTIONS_BZ_SORT_CITIES_BY_REQUIREMENT",
-        description: "LOC_OPTIONS_BZ_SORT_CITIES_BY_REQUIREMENT_DESCRIPTION",
-    });
-});
-const onInitSortCitiesBySlots = (info) => {
-    info.currentValue = bzReSortsOptions.sortCitiesBySlots;
-};
-const onUpdateSortCitiesBySlots = (_info, flag) => {
-    bzReSortsOptions.sortCitiesBySlots = flag;
-};
-Options.addInitCallback(() => {
-    Options.addOption({
-        category: CategoryType.Mods,
-        // @ts-ignore
-        group: "bz_mods",
-        type: OptionType.Checkbox,
-        id: "bz-sort-cities-by-slots",
-        initListener: onInitSortCitiesBySlots,
-        updateListener: onUpdateSortCitiesBySlots,
-        label: "LOC_OPTIONS_BZ_SORT_CITIES_BY_SLOTS",
-        description: "LOC_OPTIONS_BZ_SORT_CITIES_BY_SLOTS_DESCRIPTION",
+        id: "bz-re-sorts-group-by-req",
+        initListener: onInitGroupByReq,
+        updateListener: onUpdateGroupByReq,
+        label: "LOC_OPTIONS_BZ_RE_SORTS_GROUP_BY_REQ",
+        description: "LOC_OPTIONS_BZ_RE_SORTS_GROUP_BY_REQ_DESCRIPTION",
     });
 });
 
--- ui/resource-allocation/bz-model-resource-allocation.js
+++ ui/resource-allocation/bz-model-resource-allocation.js
@@ -1,78 +1,91 @@
 import bzReSortsOptions from '/bz-re-sorts/ui/options/bz-re-sorts-options.js';
 import ResourceAllocation from '/base-standard/ui/resource-allocation/model-resource-allocation.js';
 
-const BZ_RESOURCECLASS_SORT = {
-    RESOURCECLASS_EMPIRE: 1,
-    RESOURCECLASS_CITY: 2,
-    RESOURCECLASS_BONUS: 3,
-    RESOURCECLASS_TREASURE: 4,
-    RESOURCECLASS_FACTORY: 5,
-}
-const resourceSort = (a, b) => {
-    // first sort empire, city, bonus, treasure, factory
-    const groupA = BZ_RESOURCECLASS_SORT[a.classType] ?? 0;
-    const groupB = BZ_RESOURCECLASS_SORT[b.classType] ?? 0;
-    if (groupA != groupB) return groupA - groupB;
-    // then sort by name
-    const nameA = Locale.compose(a.name);
-    const nameB = Locale.compose(b.name);
-    return nameA.localeCompare(nameB);
-}
-const BZ_SETTLEMENT_SORT = {
-    Capital: 1,
-    City: 2,
-    Town: 3,
-}
-const settlementSort = (a, b) => {
-    if (bzReSortsOptions.sortCitiesByRequirement) {
-        // group factories
-        if (a.hasFactory && !b.hasFactory) return -1;
-        if (b.hasFactory && !a.hasFactory) return +1;
-    }
-    if (bzReSortsOptions.sortCitiesByType) {
-        // group by capital, city, town
-        const groupA = BZ_SETTLEMENT_SORT[a.settlementType] ?? 0;
-        const groupB = BZ_SETTLEMENT_SORT[b.settlementType] ?? 0;
-        if (groupA != groupB) return groupA - groupB;
-    }
-    if (bzReSortsOptions.sortCitiesByRequirement) {
-        // group city bonus types (Rail Station, Distant Lands)
-        if (a.bonusSort != b.bonusSort) return b.bonusSort - a.bonusSort;
-    }
-    if (bzReSortsOptions.sortCitiesBySlots) {
-        // sort by total resource slots
-        const groupA = a.resourceCap;
-        const groupB = b.resourceCap;
-        if (groupA != groupB) return groupB - groupA;
-    }
-    // then sort by name
-    const nameA = Locale.compose(a.name).toUpperCase();
-    const nameB = Locale.compose(b.name).toUpperCase();
+// name sorting
+const localeOrder = (a, b) => {
     // TODO: ideally this would use case-insensitive comparison instead,
     //       but the game doesn't seem to support Intl.Collator options.
     // const locale = Locale.getCurrentDisplayLocale();
     // nameA.localeCompare(nameB, locale, { sensitivity: "base" });
+    const nameA = Locale.compose(a.name).toUpperCase();
+    const nameB = Locale.compose(b.name).toUpperCase();
     return nameA.localeCompare(nameB);
+};
+
+// resource sorting
+var RClassOrder;
+(function (RClassOrder) {
+    RClassOrder[RClassOrder["RESOURCECLASS_EMPIRE"] = 0] = "RESOURCECLASS_EMPIRE";
+    RClassOrder[RClassOrder["RESOURCECLASS_CITY"] = 1] = "RESOURCECLASS_CITY";
+    RClassOrder[RClassOrder["RESOURCECLASS_BONUS"] = 2] = "RESOURCECLASS_BONUS";
+    RClassOrder[RClassOrder["RESOURCECLASS_TREASURE"] = 3] = "RESOURCECLASS_TREASURE";
+    RClassOrder[RClassOrder["RESOURCECLASS_FACTORY"] = 4] = "RESOURCECLASS_FACTORY";
+})(RClassOrder || (RClassOrder = {}));
+const resourceClassOrder = (a, b) => {
+    const groupA = RClassOrder[a.classType] ?? -1;
+    const groupB = RClassOrder[b.classType] ?? -1;
+    return groupA - groupB;
+};
+const resourceOrder = (a, b) => resourceClassOrder(a, b) || localeOrder(a, b);
+
+// settlement sorting
+var STypeOrder;
+(function (STypeOrder) {
+    STypeOrder[STypeOrder["Capital"] = 0] = "Capital";
+    STypeOrder[STypeOrder["City"] = 1] = "City";
+    STypeOrder[STypeOrder["Town"] = 2] = "Town";
+})(STypeOrder || (STypeOrder = {}));
+const settlementTypeOrder = (a, b) => {
+    if (!bzReSortsOptions.groupByType) return 0;
+    // group by capital, city, town
+    const groupA = STypeOrder[a.settlementType] ?? -1;
+    const groupB = STypeOrder[b.settlementType] ?? -1;
+    return groupA - groupB;
+};
+const factoryOrder = (a, b) => {
+    if (!bzReSortsOptions.groupByReq) return 0;
+    // group factories
+    if (a.hasFactory && !b.hasFactory) return -1;
+    if (b.hasFactory && !a.hasFactory) return +1;
+    return 0;
+};
+const bonusOrder = (a, b) => {
+    if (!bzReSortsOptions.groupByReq) return 0;
+    // sort by number of bonus conditions met
+    return b.bzBonusOrder - a.bzBonusOrder;
+};
+const slotsOrder = (a, b) => {
+    // sort by total resource slots
+    const groupA = a.resourceCap;
+    const groupB = b.resourceCap;
+    return groupA - groupB;
+}
+const sortSettlements = (list, order, direction) => {
+    if (!direction) direction = -1;  // descending
+    const settlementOrder = (a, b) =>
+        factoryOrder(a, b) || settlementTypeOrder(a, b) || bonusOrder(a, b) ||
+            direction * order(a, b) || localeOrder(a, b);
+    list.sort(settlementOrder);
 };
 const updateSettlements = (list) => {
     const age = GameInfo.Ages.lookup(Game.age);
     for (const item of list) {
-        item.currentResources.sort(resourceSort);
-        item.visibleResources.sort(resourceSort);
-        item.treasureResources.sort(resourceSort);
-        item.bonusSort = 0;
+        item.currentResources.sort(resourceOrder);
+        item.visibleResources.sort(resourceOrder);
+        item.treasureResources.sort(resourceOrder);
+        item.bzBonusOrder = 0;
         const stype = [Locale.compose(item.settlementTypeName)];
         const city = Cities.get(item.id);
         const hasBuilding = (b) => city.Constructibles?.hasConstructible(b, false);
         switch (age.ChronologyIndex) {
             case 0:  // antiquity
                 if (!city.isTown && !city.isCapital) {
-                    item.bonusSort = +1;
+                    item.bzBonusOrder = +1;
                 }
                 break;
             case 1:  // exploration
                 if (city.isDistantLands) {
-                    item.bonusSort = +1;
+                    item.bzBonusOrder = +1;
                     stype.push(Locale.compose("LOC_PLOT_TOOLTIP_HEMISPHERE_WEST"));
                 } else {
                     stype.push(Locale.compose("LOC_PLOT_TOOLTIP_HEMISPHERE_EAST"));
@@ -80,18 +93,18 @@ const updateSettlements = (list) => {
                 break;
             case 2:  // modern
                 if (hasBuilding("BUILDING_RAIL_STATION")) {
-                    item.bonusSort = +1;
+                    item.bzBonusOrder = +1;
                     stype.push(Locale.compose("LOC_BUILDING_RAIL_STATION_NAME"));
                 }
                 if (hasBuilding("BUILDING_PORT")) {
-                    item.bonusSort = +1;
+                    item.bzBonusOrder = +1;
                     stype.push(Locale.compose("LOC_BUILDING_PORT_NAME"));
                 }
                 break;
         }
         item.settlementTypeName = stype.join(" • ");
     }
-    list.sort(settlementSort);
+    sortSettlements(list, slotsOrder);
 }
 
 const initialize = () => {
@@ -100,13 +113,13 @@ const initialize = () => {
     proto.update = function(...args) {
         update.apply(this, args);
         updateSettlements(this._availableCities);
-        this._empireResources.sort(resourceSort);
-        this._uniqueEmpireResources.sort(resourceSort);
-        this._allAvailableResources.sort(resourceSort);
-        this._availableBonusResources.sort(resourceSort);
-        this._availableResources.sort(resourceSort);
-        this._availableFactoryResources.sort(resourceSort);
-        this._treasureResources.sort(resourceSort);
+        this._empireResources.sort(resourceOrder);
+        this._uniqueEmpireResources.sort(resourceOrder);
+        this._allAvailableResources.sort(resourceOrder);
+        this._availableBonusResources.sort(resourceOrder);
+        this._availableResources.sort(resourceOrder);
+        this._availableFactoryResources.sort(resourceOrder);
+        this._treasureResources.sort(resourceOrder);
     }
 };
 engine.whenReady.then(initialize);
